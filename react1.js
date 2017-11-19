9e92d7620949faa7c255ea4fe4a316e4ff15de20
Edem <edemkv@users.noreply.github.com>
2017-11-19 16:34:21 +0000

    Create react1.js

---
+++ react1.js
@@ -0,0 +1,23 @@
+class Button extends React.Component {
+	state = { counter: 0 };
+  // constructor(props){
+  //     super(props);
+  //     this.state = {counter: 0};
+  // }
+  handleClick = () => {
+    this.setState((prevState) => {
+      return {counter: prevState.counter + 1};
+    });
+  };
+  
+  render (){
+      return(
+        <button onClick={this.handleClick}> 
+        {this.state.counter}
+        </button>
+      );
+  }
+
+};
+
+ReactDOM.render(<Button/>, mountNode);
